8e72904275d7bc2e63c4c84c4017b5fff248e940
María Acevedo <77858277+maneacevedo@users.noreply.github.com>
2021-04-21 19:38:47 -0400

    Update variables.js

--- variables.js
+++ variables.js
@@ -13,14 +13,14 @@ const doesSheWantsMeToLoveHer = false;
 // UNDEFINED - Valor que tiene una variable a la cual no se le ha asignado ningún valor (aún)
 let mealsSheNeedsInADay
 // 2.
-const mealsSheWantsInADay = "undefined";
+const mealsSheWantsInADay = undefined;
 
 // NULL - La variable no tiene ningún valor. Puede referirse a un "no aplica"
-const bathingTime = "null";
+const bathingTime = null;
 
 // ARRAY - Conjunto de datos. Cada lugar en el arreglo puede ser otro tipo de dato
-let whereDidSheSleep = ("my bed", "laundry basket", "on the computer desk");
-const whereShouldSheSleep = ("Cat's bed")
+let whereDidSheSleep = ["my bed", "laundry basket", "on the computer desk"];
+const whereShouldSheSleep = ["Cat's bed"];
 
 // OBJECT - Conjunto de datos, que se estructuran en un formato llave:valor
 let pets = {
